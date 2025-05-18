fbeb6283088556653ce4ca67f03d3a19924a38c2
tonycoripe10 <antoniojimenezolmo@gmail.com>
2025-05-18 11:32:56 +0200

    main.py

--- main.py
+++ main.py
@@ -26,7 +26,17 @@ HEADERS = { "User-Agent": "Mozilla/5.0" }
 
 #--------------------------------------------
 
-def send_telegram_message(message): url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage" payload = {"chat_id": CHAT_ID, "text": message, "parse_mode": "HTML"} try: requests.post(url, data=payload) except Exception as e: print("Error enviando mensaje a Telegram:", e)
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    try:
+        requests.post(url, data=payload)
+    except Exception as e:
+        print("Error enviando mensaje a Telegram:", e)
 
 #--------------------------------------------
 
